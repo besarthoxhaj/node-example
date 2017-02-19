2f86877f8533695b1d2a15691a1caf73157c2ad8
besart <besartshyti@gmail.com>
2017-02-19 14:28:17 +0200

    create html form and serve it

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-
-
+var fs = require('fs');
 var http = require('http');
 
+var form = fs.readFileSync(__dirname+'/form.html','utf8');
+
 http.createServer(function(req,res) {
-  res.end('Hello GSG');
+  if(req.url === '/') {
+    res.end(form);
+  } else {
+    res.end('Not found');
+  }
 }).listen(process.env.PORT || 8080,function() {
   console.log('Listening on 8080');
 });
