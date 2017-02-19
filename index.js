a336cd548b0fbea188a0a80436eaaf11f917aa73
besart <besartshyti@gmail.com>
2017-02-19 14:52:08 +0200

    create util function and parse form body

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var fs = require('fs');
 var http = require('http');
+var utils = require('./utils.js');
 
 var form = fs.readFileSync(__dirname+'/form.html','utf8');
 
 http.createServer(function(req,res) {
   if(req.url === '/') {
     res.end(form);
+  } else if (
+    req.url === '/form'
+    && req.method === 'POST'
+  ) {
+    utils.parseBody(req,function(err,body){
+      var str = JSON.stringify(body);
+      res.end('Thank you! This is your mess: ' + str);
+    });
   } else {
     res.end('Not found');
   }
