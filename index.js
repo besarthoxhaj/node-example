e1e5e8155aebdae7e4f3324ca3f66b8e295690d0
besart <besartshyti@gmail.com>
2017-02-19 15:57:31 +0200

    post mess to gitter with https

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var http = require('http');
+var https = require('https');
 var utils = require('./utils.js');
 
 var form = fs.readFileSync(__dirname+'/form.html','utf8');
@@ -12,8 +13,34 @@ http.createServer(function(req,res) {
     && req.method === 'POST'
   ) {
     utils.parseBody(req,function(err,body){
+
       var str = JSON.stringify(body);
-      res.end('Thank you! This is your mess: ' + str);
+      var postData = JSON.stringify({text:body.message});
+      var dataLength = Buffer.byteLength(postData);
+
+      var options = {
+        hostname: 'api.gitter.im',
+        port: 443,
+        path: '/v1/rooms/588f114dd73408ce4f46e903/chatMessages',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Content-Length': dataLength,
+          'Authorization': 'Bearer ' + process.env.TOKEN
+        }
+      };
+
+      var request = https.request(options, function(resGitter){
+        res.end('Thank you! Your mess was sent to Gitter');
+      });
+
+      request.on('error', function(err) {
+        console.log('err',err);
+      });
+
+      request.write(postData);
+      request.end();
     });
   } else {
     res.end('Not found');
