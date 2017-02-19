e14f849c107ee0d9c06e4834dd28a9f7f2c10119
besart <besartshyti@gmail.com>
2017-02-19 13:00:53 +0200

    add npm with package json

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
+
 var http = require('http');
 
 http.createServer(function(req,res) {
   res.end('Hello GSG');
-}).listen(process.evn.PORT,function() {
+}).listen(process.env.PORT || 8080,function() {
   console.log('Listening on 8080');
 });
