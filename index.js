d90e449a6bcd41a0fdc81dc280e649fdc008525c
besart <besartshyti@gmail.com>
2017-02-19 16:57:45 +0200

    refactor

--- index.js
+++ index.js
@@ -1,48 +1,14 @@
-var fs = require('fs');
 var http = require('http');
-var https = require('https');
 var utils = require('./utils.js');
-
-var form = fs.readFileSync(__dirname+'/form.html','utf8');
+var router = require('./router.js');
 
 http.createServer(function(req,res) {
-  if(req.url === '/') {
-    res.end(form);
-  } else if (
-    req.url === '/form'
-    && req.method === 'POST'
-  ) {
-    utils.parseBody(req,function(err,body){
-
-      var str = JSON.stringify(body);
-      var postData = JSON.stringify({text:body.message});
-      var dataLength = Buffer.byteLength(postData);
-
-      var options = {
-        hostname: 'api.gitter.im',
-        port: 443,
-        path: '/v1/rooms/588f114dd73408ce4f46e903/chatMessages',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Accept': 'application/json',
-          'Content-Length': dataLength,
-          'Authorization': 'Bearer ' + process.env.TOKEN
-        }
-      };
-
-      var request = https.request(options, function(resGitter){
-        res.end('Thank you! Your mess was sent to Gitter');
-      });
-
-      request.on('error', function(err) {
-        console.log('err',err);
-      });
-
-      request.write(postData);
-      request.end();
-    });
-  } else {
+  var path = req.method + ' ' + req.url;
+  try {
+    router[path](req,res);
+  } catch(err) {
+    console.log('path',path);
+    console.log('err',err);
     res.end('Not found');
   }
 }).listen(process.env.PORT || 8080,function() {
---
+++ router.js
@@ -0,0 +1,5 @@
+
+module.exports = {
+  'GET /': require('./app/root.js'),
+  'POST /form': require('./app/form.js')
+};
